ebda9f5b59f20db05d5c0c5b31a8e81f704fd686
Md. Nayem Gawhar <nayem.gawhar@ucepbd.org>
2023-08-23 12:56:17 +0600

    build production app directory change commit

---
+++ .next/server/chunks/31.js
@@ -0,0 +1,76 @@
+exports.id = 31;
+exports.ids = [31];
+exports.modules = {
+
+/***/ 5303:
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ 3011:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6786);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2947);
+
+
+const loading = ()=>{
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: "max-w-sm animate-pulse",
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                        className: "h-8 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                        className: "sr-only",
+                        children: "Loading..."
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: "grid grid-cols-2 md:grid-cols-3 gap-4 animate-pulse",
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                        className: "h-20 bg-gray-200 rounded-lg dark:bg-gray-700 max-w-full py-4"
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                        className: "h-20 bg-gray-200 rounded-lg dark:bg-gray-700 max-w-full py-4"
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                        className: "h-20 bg-gray-200 rounded-lg dark:bg-gray-700 max-w-full py-4"
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                        className: "h-20 bg-gray-200 rounded-lg dark:bg-gray-700 max-w-full py-4"
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                        className: "h-20 bg-gray-200 rounded-lg dark:bg-gray-700 max-w-full py-4"
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                        className: "h-20 bg-gray-200 rounded-lg dark:bg-gray-700 max-w-full py-4"
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                        className: "sr-only",
+                        children: "Loading..."
+                    })
+                ]
+            })
+        ]
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (loading);
+
+
+/***/ })
+
+};
+;
---
+++ .next/static/chunks/app/contact/loading-198a09c954aa1c13.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[462,301,506,327,115,725,180,56],{3521:function(){}},function(n){n.O(0,[971,596,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
